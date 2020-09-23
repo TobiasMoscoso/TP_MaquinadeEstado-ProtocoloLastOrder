0df9c06d11373d444299eaa56f895261453f89d0
TobiasMoscoso <64030021+TobiasMoscoso@users.noreply.github.com>
2020-09-23 00:57:02 -0300

    punteros a func

--- Code/source/main.c
+++ Code/source/main.c
@@ -1,34 +1,25 @@
 /**
  * Trabajo Practico Maquinas de Estado
- * Sistema LastOrder v1.65
+ * Sistema LastOrder v1.978
  * 
 */
-//gcc main.c functions.c -o combined
+
 
 #include "../lib/prototypes.h"
 int main(){
     estados_t estado;
-    printf("***Sistema LastOrder v1.798***\n");
+    printf("***Sistema LastOrder v1.978***\n");
     //leer y cargar el archivo de configuracion
     depoGeneral_t deposito = getConfig();
-    char error;
+
     deposito_t prodAlmacenar;
     deposito_t *prod = &prodAlmacenar;
+    char error;
     char *e = &error;
     estado = 0;//Inicio "Hay algun producto"
+    estados_t (*paf[])(depoGeneral_t,char*,deposito_t*)={f_LeerMaquinas,f_dirFinalMaquina,f_dirInicialMaquina,f_error};
 
-
-    while(1){
-        switch(estado){
-            case Espera:
-                estado = f_LeerMaquinas(deposito,e,prod);break;
-            case Almacenar:
-                estado = f_dirFinalMaquina(deposito,e,prod);break;
-            case IrUbicacionInicial:
-                estado = f_dirInicialMaquina(deposito,e,prod);break;
-            case Error:
-                estado = f_error(deposito,e,prod);break;//Todas las maquina de estado necesitan de deposito?
-        }
-    }
+    while(1)
+        estado = (*paf[estado])(deposito,e,prod);
 
 }
